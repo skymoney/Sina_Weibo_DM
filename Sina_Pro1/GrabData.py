78e79829a6dc78b36e441cc415510f12debd62dc
SkyMoney <CChain0615@gmail.com>
2013-02-20 13:04:56 +0800

    Security Config for token

--- Sina_Pro1/GrabData.py
+++ Sina_Pro1/GrabData.py
@@ -4,33 +4,39 @@ Created on 2013-2-17
 
 @author: moneyc
 '''
-import urllib
+import urllib,ConfigParser
 import json
 
 url="https://api.weibo.com/2/users/show.json?uid=1648836677&access_token=2.00xj2anBcbu_3Cdac721cd58Gkbe2B"
 
-def getFriends(uid):
+def getAccessToken():
+    cf=ConfigParser.ConfigParser()
+    cf.read("property.config")
+    token=cf.get("app info","token")
+    return token
+
+def getFriends(uid,token=None):
     #get id of users who follows the user given uid
     #(uid)<-[follow]-[users]
     URL="https://api.weibo.com/2/friendships/followers/ids.json"
-    params="uid"+str(uid)+"&access_token=2.00xj2anBcbu_3Cdac721cd58Gkbe2B"
+    params="uid"+str(uid)+"&access_token="+token
     content=urllib.urlopen(URL+"?"+params)
     jsondata=json.loads(content.read())
     print jsondata
 
-def getFollowUsers(uid):
+def getFollowUsers(uid,token=None):
     #get users the user follows given uid
     #(uid)-[follow]->[users]
     URL="https://api.weibo.com/2/friendships/friends/ids.json"
-    params="uid"+str(uid)+"&access_token=2.00xj2anBcbu_3Cdac721cd58Gkbe2B"
+    params="uid"+str(uid)+"&access_token="+token
     content=urllib.urlopen(URL+"?"+params)
     jsondata=json.loads(content.read())
     print jsondata
 
-def getTags(uid):
+def getTags(uid,token=None):
     #get tags of one specified user given uid
     URL="https://api.weibo.com/2/tags.json"
-    params="uid="+str(uid)+"&access_token=2.00xj2anBcbu_3Cdac721cd58Gkbe2B"
+    params="uid="+str(uid)+"&access_token="+token
     content=urllib.urlopen(URL+"?"+params)
     jsondata=json.loads(content.read())
     for tag_single in jsondata:
@@ -40,6 +46,7 @@ if __name__=="__main__":
     content=urllib.urlopen(url)
     jsondata=content.read()
     decode_data=json.loads(jsondata)
-    print decode_data
     #getTags(decode_data["id"])
-    getFollowUsers(decode_data["id"])
+    #getFollowUsers(decode_data["id"])
+    token=getAccessToken()
+    getTags(decode_data["id"],token)
